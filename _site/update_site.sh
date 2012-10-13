f4ba4aa841ef5e780b470e183c996fa8489bdfd2
lakshmi-kannan <lakshmi.kannan@rackspace.com>
2012-10-13 15:09:29 -0700

    Whatever

---
+++ _site/update_site.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+git pull origin master
+cd _source
+jekyll --no-server
+cd ..
+rsync -vur --delete _source/_site/* .
+rm -rf _source/_site
+git add * .nojekyll
+git commit -m "Regenerate website"
