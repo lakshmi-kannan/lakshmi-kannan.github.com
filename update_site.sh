b30c0ad9e698efe524d6942de443c77ae0162dec
Lakshmi Kannan <lakshmi.kannan@rackspace.com>
2013-12-18 11:33:16 -0800

    Regenerate website

--- update_site.sh
+++ update_site.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 git pull origin master
-jekyll --no-server
-git add * .nojekyll
+cd _source
+jekyll build
+cd ..
+rsync -vur --delete _source/_site/* .
+rm -rf _source/_site
+git add . .nojekyll
 git commit -m "Regenerate website"
